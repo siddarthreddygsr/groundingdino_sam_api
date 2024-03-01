e43ec2761758abe683619528089e317911a9fc4b
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-03-02 02:07:18 +0530

    Update fooocus.py

--- fooocus.py
+++ fooocus.py
@@ -7,13 +7,13 @@ import time
 from PIL import Image as im
 
 def host_url(pod_id):
-    return f"https://{pod_id}-8888.proxy.runpod.net"
+    return f"https://{pod_id}-8628.proxy.runpod.net"
 
 def focus_endpoint(image_url,mask_url,pod_id):
 
     params={
-            "prompt": "perfect teeth, shiny teeth,veneer teeth, super white teeth, perfect shape of teeth",
-            "negative_prompt" : "imperfect teeth",
+            "prompt": "lips filler, plumper lips, large lips, beautiful perfect lips",
+            "negative_prompt" : "imperfect lips",
             "style_selections":["Fooocus V2,Fooocus Enhance,Fooocus Sharp, Fooocus Negative"],
             "require_base64":True,
             "async_process": False,
